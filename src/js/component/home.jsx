413c4cb9fa9b65b5086938624723b58a7a10b2d0
AJPadillo <98108563+AJPadillo@users.noreply.github.com>
2024-06-28 19:53:39 +0000

    pendiente estilos 1

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -6,33 +6,39 @@ const Home = () => {
 	const [inputDef, setInputDef] = useState("");
 	const [toDoList, setToDoList] = useState(['Make the bed', 'Wash my hands', 'Eat', 'Walk the dog'])
 
-	function arrayPos() {
-		for (let i = 0; i < toDoList.length; i++) {
-			const elemento = toDoList[i];
-			return elemento;
-		}
+	const arrayPos = () => {
+		return toDoList.map((elemento, index) => <li className="list-group-item d-flex justify-content-between align-items-center fs-3" key={index}>{elemento}<span
+			className="delete-btn btn btn-sm fs-3"
+			onClick={() => deleteItem(index)}
+		>
+			&times;
+		</span></li>);
 	}
 
 	const onSubmit = (e) => {
 		e.preventDefault();
-		const newList = [inputDef];
+		const newList = [...toDoList, inputDef];
 		setToDoList(newList);
 		setInputDef("");
 	}
 
+	const deleteItem = (index) => {
+		const newList = toDoList.filter((_, i) => i !== index);
+		setToDoList(newList);
+	};
+
 	return (
-		<>
+		<div className="container">
+			<h1>todo</h1>
 			<form onSubmit={onSubmit}>
-				<div className="mb-3">
-					<input type="text" className="form-control" placeholder="What needs to be done?" value={inputDef} onChange={(e) => setInputDef(e.target.value)} />
+				<div className="">
+					<input type="text" className="form-control rounded-0 border-bottom-0 fs-3" placeholder="What needs to be done?" value={inputDef} onChange={(e) => setInputDef(e.target.value)} />
 				</div>
 			</form>
-			<ul>
-				{arrayPos()}
-				{arrayPos()}
+			<ul className="list-group rounded-0" style={{ listStyle: "none" }}>
 				{arrayPos()}
 			</ul>
-		</>
+		</div>
 	);
 };
 
