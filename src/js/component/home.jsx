2700af73e09b66e38c4a000a9968bc2ace494717
AJPadillo <98108563+AJPadillo@users.noreply.github.com>
2024-07-01 10:14:38 +0000

    terminado 2

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -7,7 +7,7 @@ const Home = () => {
 	const [toDoList, setToDoList] = useState(['Make the bed', 'Wash my hands', 'Eat', 'Walk the dog'])
 
 	const arrayPos = () => {
-		return toDoList.map((elemento, index) => <li className="list-group-item d-flex justify-content-between align-items-center fs-3" key={index}>{elemento}<span
+		return toDoList.map((elemento, index) => <li className="list-group-item d-flex justify-content-between align-items-center fs-3 ps-5" key={index}>{elemento}<span
 			className="delete-btn btn btn-sm fs-3"
 			onClick={() => deleteItem(index)}
 		>
@@ -33,11 +33,12 @@ const Home = () => {
 			<div className="mx-4">
 				<form onSubmit={onSubmit} className="shadow-lg">
 					<div>
-						<input type="text" className="form-control rounded-0 border-bottom-0 fs-2" placeholder="What needs to be done?" value={inputDef} onChange={(e) => setInputDef(e.target.value)} />
+						<input type="text" className="form-control rounded-0 border-bottom-0 fs-2 ps-5" placeholder="What needs to be done?" value={inputDef} onChange={(e) => setInputDef(e.target.value)} />
 					</div>
 				</form>
 				<ul className="list-group rounded-0" style={{ listStyle: "none" }}>
 					{arrayPos()}
+					<p className="list-group-item">{toDoList.length > 0 ? toDoList.length + " item(s) left" : "No hay tareas, añadir tareas"}</p>
 				</ul>
 			</div>
 
